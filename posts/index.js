cbdd53a3455f24a94f6cbc8c930b063fa1ffeb5d
korbiwe <nikita.stepanchenko@fora-soft.com>
2020-06-25 22:08:38 +0300

    services for communicating between microservices

--- posts/index.js
+++ posts/index.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 
 const posts = {};
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const { title } = req.body;
   const id = randomBytes(4).toString('hex');
 
@@ -20,7 +20,7 @@ app.post('/posts', async (req, res) => {
     id, title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id, title
@@ -37,5 +37,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log('v2.0');
   console.log('Listening on 4000');
 });
